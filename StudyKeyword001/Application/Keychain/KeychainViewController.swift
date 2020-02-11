e939de5a2a898f27df947c66daeee59fe7e384b1
juanagustin0504 <juanagustin@naver.com>
2020-02-11 18:13:45 +0900

    - add description

--- StudyKeyword001/Application/Keychain/KeychainViewController.swift
+++ StudyKeyword001/Application/Keychain/KeychainViewController.swift
@@ -6,6 +6,49 @@
 //  Copyright © 2020 Moonift. All rights reserved.
 //
 
+/*
+ Keychain
+
+ - [ios] Keychain으로 안전하게 데이터 저장하기
+
+ UserDefaults는 단순한 데이터를 저장하는데는 문제가 없지만 민감한 비밀번호나, 인증서 같은 정보들, 혹은 사용자가 생각하기에 본인만의 민감한 정보들을 저장하기에는 안전하지 않다 -> Keychain을 이용하여 데이터 저장
+
+ Keychain이란?
+ 민감한 데이터들을 안전하게 저장하는 것은 굉장히 복잡한 암호화 절차를 필요로 합니다. Keychain이라는 서비스를 사용합니다.
+
+ Keychain은 하나의 암호화된 컨테이너이며 Keychain services API는 이처럼 민감한 데이터를 암호화하고 복호화하며 재사용하는 행위를 보다 쉽고 안전하게 사용할 수 있게끔 API를 제공합니다. 밑의 그림처럼 저장되는 단위를 Item이라 부릅니다.
+
+ 기본적으로 디바이스를 잠그면(Lock) Keychain 역시 잠기고 디바이스를 풀면(Unlock) Keychain 역시 풀립니다. 이렇게 Keychain이 잠긴 상태에서는 Item들에 접근할 수도 복호화할 수도 없습니다. 또한 풀린 상태에서도 해당 Item을 생성하고 저장한 어플리케이션에서만 접근이 가능합니다.
+
+ 하지만 지정된 그룹에 속한 어플리케이션끼리는 데이터에 접근할 수 있습니다.
+
+ Keychain Items
+ 민감한 정보를 저장할 때 해당 데이터를 Keychain Item으로 패키징합니다. 이렇게 패키징된 Item안에는 저장하려는 데이터뿐만 아니라 해당 데이터의 속성(Attributes)또한 같이 저장됩니다. 이 속성으로는 데이터의 접근 가능성을 제어하고 해당 Item이 검색에 노출되게끔 공개적으로 보여주는 속성입니다.
+
+ 그리고 추후에 검증된 프로세스가 Item에 접근할 때 속성의 검색을 통해 해당 데이터를 찾고 접근할 수 있습니다.
+
+ Keychain Wrappers
+ 기본적으로 제공되는 Keychain Services API도 존재하지만 이를 보다 편하고 안전하게 이용할 수 있게끔 해주는 라이브러리들이 존재합니다. 대표적인 라이브러리들은 다음과 같습니다.
+ 1. SwiftKeychainWrapper
+ 2. SAMKeychain - (Obj - c)
+ 3. LockSmith
+
+ SwiftKeychainWrapper
+ - add a string value to keychain
+ let saveSuccessful: Bool = KeychainWrapper.standard.set(“Some String”, forKey: “myKey”)
+
+ - retrieve a string value from keychain
+ let retrievedString: String? = KeychainWrapper.standard.string(forKey: “myKey”)
+
+ - remove a string value from keychain
+ let removeSuccessful: Bool = KeychainWrapper.standard.remove(key: “myKey”)
+
+ keychain을 사용할 때 :
+ - 민감한 비밀번호나 인증서를 저장할 때 사용한다.
+ - 보안성 👍
+ - iOS 장치 상에서 암호화 정보를 저장하기 위해 keychain 사용
+ */
+
 import UIKit
 import SwiftKeychainWrapper
 
@@ -72,4 +115,3 @@ class KeychainViewController: UIViewController {
     }
     
 }
-
--- StudyKeyword001/Application/UserDefaults/UserDefaultsViewController.swift
+++ StudyKeyword001/Application/UserDefaults/UserDefaultsViewController.swift
@@ -6,6 +6,51 @@
 //  Copyright © 2020 Moonift. All rights reserved.
 //
 
+/*
+ UserDefault
+ 
+ -  데이터 저장소
+
+ UserDefaults는 사용자 기본 설정과 같은 단일 데이터 값에 적합하다.
+
+ 그래서 대량의 유사한 데이터(테이블에 대한 레코드, 여러 사용자에 대한 데이터 등)를 저장해야하는 경우에는 sqlite 데이터베이스가 더 적합하다.
+
+ UserDefaults는 [데이터, 키(key)]으로 데이터를 저장한다.
+ 이때 key값은 String이다.
+
+ 대체로 standard라는 이름의 싱글톤을 이용해 데이터에 액세스하기 때문에 인스턴스를 생성할 필요가 없다.
+
+ 값을 저장할 때 :
+ UserDefaults.standard.set(value, forKey: ‘Some Key Name”)
+ UserDefaults.synchronize()
+
+ synchronize() 메소드는 UserDefaults에 기록된 값을 파일에쓰는 역할을 한다.
+ 따라서 여러 값을 변경할 경우 매번 synchronize()를 호출할 필요 없이 다 쓰고 난 다음에 마지막에 딱 한번만 synchronize()를 호출하면 된다.
+
+ 호출하지 않아도 특정 인터벌이나 클래스 deinit 시를 기준으로 synchronize() 알아서 호출한다.
+
+ nil을 넣어버리면 해당 데이터를 지우는 것과 동일하다.
+
+ 저장한 값을 읽어올 때:
+ let value = UserDefaults.standard.object(forKey: “Some Key Name”) as? String
+
+ object로 가져오면 타입을 확실하게 바꿔주어야 할 것이고
+ or
+ let value = UserDefaults.standard.string(forKey: “Some Key Name”)
+ 처럼 타입을 명확하게 해서 읽어올 수도 있다.
+
+ 읽기 시에는 저장된 값이 없을수도 있어서 읽기 메소드는 모두 옵셔널을 리턴한다.
+ 반대로 이를 이용해 기본값을 제공하는 방법도 있다.
+
+ vara userName: String {
+     return UserDefaults.standard.string(forKey: “User Name”) ?? “Noname”
+ }
+
+ 프로퍼티 getter를 이런 식으로 구현하는 것도 가능하다.
+
+
+ */
+
 import UIKit
 
 class UserDefaultsViewController: UIViewController {
